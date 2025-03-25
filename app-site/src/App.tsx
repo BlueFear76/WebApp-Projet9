d4bb4c15e7dcd1c2ee2c307986d98433587a2921
BlueFear <bluefear1976@gmail.com>
2025-03-25 15:40:28 +0100

    modif login

--- app-site/src/App.tsx
+++ app-site/src/App.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link, Navigate } from 'react-router-dom';
 import './App.css';
 import Authentification from './pages/authentificationPage';
 import Home from './pages/homePage';
 import Tool from './pages/toolPage';
 
-function App() {
+const App: React.FC = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
   return (
     <Router>
+      {isAuthenticated && (
         <div className="nav-links">
           <Link to="/tools">
             <span className="page-text">Tools</span>
@@ -15,17 +18,26 @@ function App() {
           <Link to="/home">
             <span className="page-text">Home</span>
           </Link>
-          <Link to="/">
-            <span className="page-text">Se Deconnecter</span>
+          <Link to="/" onClick={() => setIsAuthenticated(false)}>
+            <span className="page-text">Se Déconnecter</span>
           </Link>
         </div>
+      )}
 
-      {/* Contenu principal décalé */}
       <div className="main-content">
         <Routes>
-          <Route path="/" element={<Authentification />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/tools" element={<Tool />} />
+          <Route 
+            path="/" 
+            element={<Authentification onLogin={() => setIsAuthenticated(true)} />} 
+          />
+          <Route 
+            path="/home" 
+            element={isAuthenticated ? <Home /> : <Navigate to="/" />} 
+          />
+          <Route 
+            path="/tools" 
+            element={isAuthenticated ? <Tool /> : <Navigate to="/" />} 
+          />
         </Routes>
       </div>
     </Router>
