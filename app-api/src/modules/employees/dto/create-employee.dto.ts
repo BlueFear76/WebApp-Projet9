bc3860e8bbce73a4a647831ad49a596ba99be3fa
godfr <gowusu018@gmail.com>
2025-03-26 22:43:15 +0100

    added documentation

--- app-api/src/modules/employees/dto/create-employee.dto.ts
+++ app-api/src/modules/employees/dto/create-employee.dto.ts
@@ -1,10 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsNotEmpty, MinLength } from 'class-validator';
 
 export class CreateEmployeeDto {
+  @ApiProperty({
+    description: 'The username of the employee',
+    example: 'JohnDoe',
+  })
   @IsString()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty({
+    description: 'The password of the employee',
+    example: 'password123',
+  })
   @IsString()
   @IsNotEmpty()
   @MinLength(6)
--- app-api/src/modules/employees/dto/login-employee.dto.ts
+++ app-api/src/modules/employees/dto/login-employee.dto.ts
@@ -1,10 +1,20 @@
+import { Employee } from './../employees.entity';
 import { IsString, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
-export class LoginDto {
+export class EmployeeLoginDto {
+  @ApiProperty({
+    description: 'The username of the employee',
+    example: 'JohnDoe',
+  })
   @IsString()
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty({
+    description: 'The password of the employee',
+    example: 'password123',
+  })
   @IsString()
   @IsNotEmpty()
   password: string;
