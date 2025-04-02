67ad8fa4174fa27282b34c45e939d0b7689ab1b9
BleuennPigeon <113584899+BleuennPigeon@users.noreply.github.com>
2025-04-02 14:54:47 +0200

    tableau tool

--- app-site/src/pages/toolPage.tsx
+++ app-site/src/pages/toolPage.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import '../styles/toolPageStyle.css';
 import { Link } from 'react-router-dom';
 
 interface Tool {
@@ -11,19 +12,41 @@ interface Tool {
 }
 
 export default function Tool() {
-  const [tools, setTools] = useState<Tool[]>([]);
-
+  //const [tools, setTools] = useState<Tool[]>([]);
+/*
   useEffect(() => {
     fetch('http://localhost:3001/tools')
       .then(response => response.json())
       .then(data => setTools(data))
       .catch(error => console.error('Erreur de chargement des outils:', error));
   }, []);
+  */
+
+  //Données fictives des outils (à remplacer par les données réelles)
+  const fakeTools: Tool[] = [
+    {
+      id_tool: 1,
+      status: "active",
+      type: "Pelle",
+      last_known_location_id: "Place d'Armes, 78000 Versailles",
+      tag_id: 201,
+      mission_id: "1",
+    },
+    {
+      id_tool: 2,
+      status: "maintenance",
+      type: "Tronçonneuse",
+      last_known_location_id: "Evry-Garden-12",
+      tag_id: 202,
+      mission_id: "2",
+    },
+  ];
+
 
   return (
-    <div>
-      <h1>TOOL PAGE</h1>
-      <table style={{ width: '100%', borderCollapse: 'collapse', border: '1px solid black'}}>
+    <div className="toolPage">
+      <h1 className="title">TOOL PAGE</h1>
+      <table className="tools-table">
         <thead>
           <tr>
             <th>ID</th>
@@ -35,7 +58,7 @@ export default function Tool() {
           </tr>
         </thead>
         <tbody>
-          {tools.map((tool) => (
+          {fakeTools.map((tool) => (
             <tr key={tool.id_tool}>
               <td>{tool.id_tool}</td>
               <td>{tool.status}</td>
