8c864cfa46678d1cd2e529c8b1c1dd67ac02b741
BleuennPigeon <113584899+BleuennPigeon@users.noreply.github.com>
2025-04-15 15:57:47 +0200

    responsive

--- app-site/src/App.tsx
+++ app-site/src/App.tsx
@@ -7,15 +7,11 @@ import Tool from './pages/toolPage';
 import MissionPage from './pages/missonPage';
 import EmployeePage from './pages/employeePage';
 
-import profilImage from './images/profil.svg'
 import homeIcon from './images/home.svg';
 import missionIcon from './images/mission.svg';
 import toolIcon from './images/tool.svg';
 import employeeIcon from './images/employee.svg';
 import logoutIcon from './images/logout.svg';
-
-
-import userEvent from '@testing-library/user-event';
 
 interface User {
   firstname: string;
@@ -39,70 +35,50 @@ const App: React.FC = () => {
     <Router>
       {isAuthenticated && (
         <div className="sidebar">
-          <h2>Navigation</h2>
           <ul>
-            <div className="page-nav">
+            <li>
+              <Link to="/home">
+                <img src={homeIcon} alt="Home" className="nav-icon" />
+                <span>Home</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/missions">
+                <img src={missionIcon} alt="Missions" className="nav-icon" />
+                <span>Missions</span>
+              </Link>
+            </li>
+            <li>
+              <Link to="/tools">
+                <img src={toolIcon} alt="Outils" className="nav-icon" />
+                <span>Outils</span>
+              </Link>
+            </li>
+            {userLogged?.role === "superAdmin" && (
               <li>
-                <Link to="/home">
-                  <img src={homeIcon} alt="Home" className="nav-icon" />
-                  <span>Home</span>
+                <Link to="/employees">
+                  <img src={employeeIcon} alt="Employés" className="nav-icon" />
+                  <span>Employés</span>
                 </Link>
               </li>
-              <li>
-                <Link to="/missions">
-                  <img src={missionIcon} alt="Missions" className="nav-icon" />
-                  <span>Missions</span>
-                </Link>
-              </li>
-              <li>
-                <Link to="/tools">
-                  <img src={toolIcon} alt="Outils" className="nav-icon" />
-                  <span>Outils</span>
-                </Link>
-              </li>
-              {userLogged.role === "superAdmin" && (
-                <li>
-                  <Link to="/employees">
-                    <img src={employeeIcon} alt="Employés" className="nav-icon" />
-                    <span>Employés</span>
-                  </Link>
-                </li>
-              )}
-            </div>
-            <div className='disconnect'>
-              <li>
-                <Link to="/" onClick={() => setIsAuthenticated(false)}>
-                 <img src={logoutIcon} alt="Outils" className="nav-icon" />
-                 <span>Déconnexion</span>
-                </Link>
-              </li>
-            </div>
+            )}
+            <li className="disconnect">
+              <Link to="/" onClick={() => setIsAuthenticated(false)}>
+                <img src={logoutIcon} alt="Déconnexion" className="nav-icon" />
+                <span>Déconnexion</span>
+              </Link>
+            </li>
           </ul>
         </div>
       )}
 
       <div className="main-content">
         <Routes>
-          <Route 
-            path="/" 
-            element={<Authentification onLogin={handleLogin} />} 
-          />
-          <Route 
-            path="/home" 
-            element={isAuthenticated ? <Home user={userLogged}/> : <Navigate to="/" />} 
-          />
-          <Route 
-            path="/tools" 
-            element={isAuthenticated ? <Tool /> : <Navigate to="/" />} 
-          />
-          <Route 
-            path="/missions" 
-            element={isAuthenticated ? <MissionPage /> : <Navigate to="/" />} 
-          />
-          <Route
-            path="/employees"
-            element={isAuthenticated ? <EmployeePage /> : <Navigate to="/"/>}
-          />
+          <Route path="/" element={<Authentification onLogin={handleLogin} />} />
+          <Route path="/home" element={isAuthenticated ? <Home user={userLogged}/> : <Navigate to="/" />} />
+          <Route path="/tools" element={isAuthenticated ? <Tool /> : <Navigate to="/" />} />
+          <Route path="/missions" element={isAuthenticated ? <MissionPage /> : <Navigate to="/" />} />
+          <Route path="/employees" element={isAuthenticated ? <EmployeePage /> : <Navigate to="/" />} />
         </Routes>
       </div>
     </Router>
