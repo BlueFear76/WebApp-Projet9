b60207123242fd9a7542e46b4fbec6528d338b7f
godfr <gowusu018@gmail.com>
2025-04-18 03:43:32 +0200

    fixed env error

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT || 3002, "0.0.0.0");
+  await app.listen(Number(process.env.PORT) , "0.0.0.0");
+
 }
 bootstrap();
